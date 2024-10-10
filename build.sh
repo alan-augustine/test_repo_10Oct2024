68cd379a5335e55a8ccaf5cfa98ccc8d763ed88a
alan-augustine <alanaugustine007@gmail.com>
2024-10-10 11:32:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+
 echo "Message from build.sh"
+
+mkdir -p artifacts
+cd artifacts
+
+echo "AAAAAAAAAAAA" > my_artifact.txt
+
+TIME_STAMP=$(date +'%Y%m%d_%H%M%S')
+zip my_artifact_${TIME_STAMP}.zip  my_artifact.txt
+
+rm -f my_artifact.txt
+
+cd ..
+echo "Directory contents post build phase: "
+ls -R .
